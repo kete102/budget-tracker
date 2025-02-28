fc5a1bea8a1b790faaadfadf65f87888d21e0f7d
Flavius Catalin Neacsu <kete102@gmail.com>
2025-03-01 00:22:14 +0100

    fix: import error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,40 +1,54 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
 import PrivatePagesLayout from '@/components/layouts/private-pages-layout'
 import PublicPagesLayout from '@/components/layouts/public-pages-layout'
-import { RootLayout } from '@/components/layouts/root-layout'
 import SignInPage from '@/pages/auth-pages/signin-page'
 import SignUpPage from '@/pages/auth-pages/signup-page'
 import HomePage from '@/pages/home-page'
 import NotFound from '@/pages/not-found'
 import AuthProvider from '@/providers/auth-provider'
 import DashboardPage from '@/pages/dashboard-page'
+import RootLayout from './components/layouts/root-layout'
 
 function App() {
+	return (
+		<BrowserRouter>
+			<AuthProvider>
+				<RootLayout>
+					<Routes>
+						{/* Public Routes*/}
+						<Route element={<PublicPagesLayout />}>
+							<Route
+								path='/'
+								element={<HomePage />}
+							/>
+							<Route
+								path='/sign-in'
+								element={<SignInPage />}
+							/>
+							<Route
+								path='/sign-up'
+								element={<SignUpPage />}
+							/>
+						</Route>
 
-  return (
-    <BrowserRouter>
-      <AuthProvider>
-        <RootLayout>
-          <Routes>
-            {/* Public Routes*/}
-            <Route element={<PublicPagesLayout />}>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/sign-in" element={<SignInPage />} />
-              <Route path="/sign-up" element={<SignUpPage />} />
-            </Route>
-
-            {/* Private Routes*/}
-            <Route element={<PrivatePagesLayout />}>
-              <Route path="/dashboard" element={<DashboardPage />} />
-              <Route path="/transactions" />
-              <Route path="/user-settings" />
-            </Route>
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </RootLayout>
-      </AuthProvider>
-    </BrowserRouter>
-  )
+						{/* Private Routes*/}
+						<Route element={<PrivatePagesLayout />}>
+							<Route
+								path='/dashboard'
+								element={<DashboardPage />}
+							/>
+							<Route path='/transactions' />
+							<Route path='/user-settings' />
+						</Route>
+						<Route
+							path='*'
+							element={<NotFound />}
+						/>
+					</Routes>
+				</RootLayout>
+			</AuthProvider>
+		</BrowserRouter>
+	)
 }
 
 export default App
