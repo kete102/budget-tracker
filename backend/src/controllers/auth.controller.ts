71b232fd33ad3a285deaedde18330316f96f7529
Flavius Catalin Neacsu <kete102@gmail.com>
2025-03-01 00:21:08 +0100

    feat: auth use cases

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,62 +1,105 @@
-import { z } from "zod";
-import bcrypt from 'bcryptjs'
-import { userTable, PublicUser } from "../db/schemas/user.schema";
-import { db } from "../drizzle";
-import { PostgresError } from "postgres";
+import { Request, Response } from 'express'
+import { generateAccessToken, generateRefreshToken } from '../lib/generateJWT'
+import { loginSchema, registerSchema, UserModel } from '../models/user.model'
+import { processEnv } from '../config'
+import jwt, { VerifyErrors } from 'jsonwebtoken'
+import { User } from '../db/schemas/user.schema'
 
-export const loginSchema = z.object({
-	email: z.string().email(),
-	username: z.string().min(3),
-	password: z.string().min(6),
-});
+export async function registerUser(req: Request, res: Response) {
+	const { email, username, password } = req.body
+	//NOTE: Validate the body
+	const validation = registerSchema.safeParse({
+		email,
+		username,
+		password,
+	})
 
-export class UserModel {
-	static async create({ email, username, password }: { email: string, username: string, password: string }) {
-		try {
-			//NOTE: Validate the body
-			const validation = loginSchema.safeParse({ email, username, password })
-
-			if (!validation.success) {
-				console.log(validation.error.message.toString())
-				throw new Error("Fields validation error")
-			}
-
-			//NOTE: Hash the password
-			const hash_password = await bcrypt.hash(validation.data.password, 10)
-
-			//NOTE: Save the user in the db
-			const [savedUser] = await db.insert(userTable).values({
-				email: validation.data.email,
-				username: validation.data.username,
-				password_hash: hash_password
-			}).returning()
-
-			if (!savedUser) {
-				throw new Error('Failed to create user')
-			}
-
-			//NOTE: Crear jwt acccesToken
-
-			//NOTE: Destructure the public user data to return it
-			const publicUser: PublicUser = {
-				id: savedUser.id,
-				email: savedUser.email,
-				username: savedUser.username
-			}
-
-			return publicUser
-		} catch (error) {
-			if (error instanceof PostgresError && error.code === '23505') {
-				throw new Error('User already exists')
-			}
-
-			if (error instanceof PostgresError) {
-				throw new Error(`Postgres error ${error.message}`)
-			}
-
-			throw new Error('Unexpected error')
-		}
+	if (!validation.success) {
+		console.log(validation.error.message.toString())
+		return res.status(400).json({ message: 'Fields validation error' })
 	}
 
-}
+	console.log('Create user')
+	const user = await UserModel.create({ email, username, password })
+	console.log('User created: ', user)
 
+	//NOTE: Crear jwt acccesToken
+	const accessToken = generateAccessToken({
+		id: user.id,
+		email: user.email,
+		username: user.username,
+	})
+
+	const refreshToken = generateRefreshToken({ id: user.id })
+
+	res.cookie('refresh_token', refreshToken, {
+		httpOnly: true,
+		secure: processEnv.NODE_ENV === 'production',
+		sameSite: 'strict',
+		maxAge: 7 * 24 * 60 * 60 * 1000,
+	})
+
+	return res.status(200).json({ accessToken })
+}
+export async function refreshToken(req: Request, res: Response) {
+	const refreshToken = req.cookies.refresh_token
+	if (!refreshToken) return res.status(401).json({ message: 'Unauthorized' })
+
+	jwt.verify(
+		refreshToken,
+		process.env.REFRESH_TOKEN_SECRET,
+		(err: VerifyErrors, user: User) => {
+			if (err) return res.status(403).json({ message: 'Invalid token' })
+
+			const newAccessToken = generateAccessToken({
+				id: user.id,
+				email: user.email,
+				username: user.username,
+			})
+
+			const newRefreshToken = generateRefreshToken({ id: user.id })
+
+			res.cookie('refresh_token', newRefreshToken, {
+				httpOnly: true,
+				secure: process.env.NODE_ENV === 'production',
+				sameSite: 'strict',
+				maxAge: 7 * 24 * 60 * 60 * 1000,
+			})
+
+			return res.status(200).json({ newAccessToken })
+		}
+	)
+}
+export async function loginUser(req: Request, res: Response) {
+	const { email, password } = req.body
+	//NOTE: Validate the body
+	const validation = loginSchema.safeParse({
+		email,
+		password,
+	})
+
+	if (!validation.success) {
+		console.log(validation.error.message.toString())
+		return res.status(400).json({ message: 'Fields validation error' })
+	}
+
+	const user = await UserModel.login({ email, password })
+
+	//NOTE: Crear jwt acccesToken
+	const accessToken = generateAccessToken({
+		id: user.id,
+		email: user.email,
+		username: user.username,
+	})
+
+	const refreshToken = generateRefreshToken({ id: user.id })
+
+	res.cookie('refresh_token', refreshToken, {
+		httpOnly: true,
+		secure: processEnv.NODE_ENV === 'production',
+		sameSite: 'strict',
+		maxAge: 7 * 24 * 60 * 60 * 1000,
+	})
+
+	return res.status(200).json({ accessToken })
+}
--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,13 +1,109 @@
-import express from 'express'
-import { registerUser } from '../controllers/auth.controller'
-import { verifyAccessToken } from '../middlewares/verifyJWT'
+import { z } from 'zod'
+import bcrypt from 'bcryptjs'
+import { userTable, PublicUser } from '../db/schemas/user.schema'
+import { db } from '../drizzle'
+import { PostgresError } from 'postgres'
+import { eq } from 'drizzle-orm'
 
-const authRouter = express.Router()
+export const registerSchema = z.object({
+	email: z.string().email(),
+	username: z.string().min(3),
+	password: z.string().min(6),
+})
 
-authRouter.post('/register', registerUser)
-authRouter.post('/login', registerUser)
-authRouter.post('/logout', registerUser)
-authRouter.post('/refresh', verifyAccessToken, registerUser)
+export const loginSchema = registerSchema.omit({ username: true })
 
-export default authRouter
+export class UserModel {
+	static async create({
+		email,
+		username,
+		password,
+	}: {
+		email: string
+		username: string
+		password: string
+	}) {
+		try {
+			//NOTE: Hash the password
+			const hash_password = await bcrypt.hash(password, 10)
 
+			//NOTE: Save the user in the db
+			const [savedUser] = await db
+				.insert(userTable)
+				.values({
+					email: email,
+					username: username,
+					password_hash: hash_password,
+				})
+				.returning()
+
+			if (!savedUser) {
+				throw new Error('Failed to create user')
+			}
+
+			//NOTE: Destructure the public user data to return it
+			const publicUser: PublicUser = {
+				id: savedUser.id,
+				email: savedUser.email,
+				username: savedUser.username,
+			}
+
+			return publicUser
+		} catch (error) {
+			if (error instanceof PostgresError && error.code === '23505') {
+				throw new Error('User already exists')
+			}
+
+			if (error instanceof PostgresError) {
+				throw new Error(`Postgres error ${error.message}`)
+			}
+
+			throw new Error('Unexpected error')
+		}
+	}
+	static async login({
+		email,
+		password,
+	}: {
+		email: string
+		password: string
+	}) {
+		try {
+			//NOTE: check existing user
+			const [existingUser] = await db
+				.select()
+				.from(userTable)
+				.where(eq(userTable.email, email))
+				.limit(1)
+
+			if (!existingUser) {
+				throw new Error('User not found')
+			}
+			//NOTE: check hash password
+			const isPasswordValid = await bcrypt.compare(
+				password,
+				existingUser.password_hash
+			)
+
+			if (!isPasswordValid) {
+				throw new Error('Incorrect password')
+			}
+
+			// NOTE: Return the public user data
+			const publicUser: PublicUser = {
+				id: existingUser.id,
+				email: existingUser.email,
+				username: existingUser.username,
+			}
+
+			return publicUser
+		} catch (error) {
+			if (error instanceof PostgresError) {
+				throw new Error(`Postgres error ${error.message}`)
+			}
+
+			throw new Error('Unexpected error')
+		}
+	}
+	static async logout() {}
+}
