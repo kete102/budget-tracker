f739f0d2b7580541ba72b52f438971836a601988
Flavius Catalin Neacsu <kete102@gmail.com>
2025-03-01 00:21:23 +0100

    feat: generateJWT helper function

--- backend/src/lib/generateJWT.ts
+++ backend/src/lib/generateJWT.ts
@@ -1,10 +1,18 @@
 import jwt from 'jsonwebtoken'
-import { processEnv } from "../config"
+import { processEnv } from '../config'
 
 const JWT_ACCESS = processEnv.JWT_SECRET_ACCESS
 const JWT_REFRESH = processEnv.JWT_SECRET_REFRESH
 
-export const generateAccessToken = ({ id, email, username }: { id: string, email: string, username: string }) => {
+export const generateAccessToken = ({
+	id,
+	email,
+	username,
+}: {
+	id: string
+	email: string
+	username: string
+}) => {
 	return jwt.sign({ id, email, username }, JWT_ACCESS, { expiresIn: '15m' })
 }
 export const generateRefreshToken = ({ id }: { id: string }) => {
