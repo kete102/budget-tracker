a3b66ba1eb2d8cb27c2556c84511db6cb7f1c267
Flavius Catalin Neacsu <kete102@gmail.com>
2025-03-01 23:45:31 +0100

    feat: check user auth

--- frontend/src/components/layouts/private-pages-layout.tsx
+++ frontend/src/components/layouts/private-pages-layout.tsx
@@ -1,18 +1,8 @@
 import Header from '../header'
 import Footer from '../footer'
-import { Outlet, useNavigate } from 'react-router'
-import { useAuth } from '@/hooks/useAuth'
-import { useEffect } from 'react'
+import { Outlet } from 'react-router'
 
 const PrivatePagesLayout = () => {
-	const { token } = useAuth()
-	const navigate = useNavigate()
-
-	useEffect(() => {
-		if (!token) {
-			navigate('/sign-in', { replace: true })
-		}
-	}, [navigate, token])
 
 	return (
 		<div className='h-full flex flex-col w-full p-4'>
--- frontend/src/components/layouts/public-pages-layout.tsx
+++ frontend/src/components/layouts/public-pages-layout.tsx
@@ -1,8 +1,13 @@
-import { Outlet } from 'react-router'
+import { Navigate, Outlet } from 'react-router'
 import Header from '../header'
 import Footer from '../footer'
+import { useAuth } from '@/hooks/useAuth'
 
 const PublicPagesLayout = () => {
+	const { token } = useAuth()
+
+	if (token) return <Navigate to='/dashboard' replace />
+
 	return (
 		<div className='h-full flex flex-col w-full p-4'>
 			<Header />
