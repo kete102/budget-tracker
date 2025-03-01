10da075bf099ad56b08e3f062bb12293980eca00
Flavius Catalin Neacsu <kete102@gmail.com>
2025-03-01 23:53:36 +0100

    feat: modify apiClient interceptors based on auth tokens

--- frontend/src/providers/auth-provider.tsx
+++ frontend/src/providers/auth-provider.tsx
@@ -1,5 +1,6 @@
 import { AuthContext } from '@/contexts/auth'
-import { ReactNode, useState } from 'react'
+import apiClient from '@/services/api/api-client'
+import { ReactNode, useEffect, useLayoutEffect, useState } from 'react'
 
 const AuthProvider = ({ children }: { children: ReactNode }) => {
 	const [token, setToken] = useState<string | null>(null)
@@ -11,6 +12,66 @@ const AuthProvider = ({ children }: { children: ReactNode }) => {
 	const logout = () => {
 		setToken(null)
 	}
+
+	useEffect(() => {
+		const fetchMe = async () => {
+			try {
+				const response = await apiClient.get('/auth/check-auth')
+				setToken(response.data.accessToken)
+			} catch {
+				setToken(null)
+			}
+		}
+
+		fetchMe()
+	}, [])
+
+	useLayoutEffect(() => {
+		const authInterceptor = apiClient.interceptors.request.use((config) => {
+			if (token) {
+				config.headers.Authorization = `Bearer ${token}`
+			}
+			return config
+		})
+
+		return () => {
+			apiClient.interceptors.request.eject(authInterceptor)
+		}
+	}, [token])
+
+	useLayoutEffect(() => {
+		const refreshInterceptor = apiClient.interceptors.response.use(
+			(response) => response,
+			async (error) => {
+				const originalRequest = error.config
+
+				if (
+					error.response.status === 403 &&
+					error.response.data.message === 'Unauthorized'
+				) {
+					if (originalRequest._retry) return Promise.reject(error)
+					originalRequest._retry = true
+
+					try {
+						const response = await apiClient.get("/auth/refresh")
+
+						setToken(response.data.accessToken)
+
+						originalRequest.headers.Authorization = `Bearer ${response.data.accessToken}`
+
+						return apiClient(originalRequest)
+					} catch (refreshError) {
+						setToken(null)
+						return Promise.reject(refreshError)
+					}
+				}
+				return Promise.reject(error)
+			}
+		)
+		return () => {
+			apiClient.interceptors.response.eject(refreshInterceptor)
+		}
+	},)
 
 	return (
 		<AuthContext.Provider value={{ token, login, logout }}>
--- frontend/src/services/api/api-client.ts
+++ frontend/src/services/api/api-client.ts
@@ -1,8 +1,5 @@
 import { processEnv } from '@/config'
-import toast from 'react-hot-toast'
-import { useAuth } from '@/hooks/useAuth'
 import axios from 'axios'
-import { useNavigate } from 'react-router'
 
 const BASE_URL =
 	processEnv.MODE === 'development'
@@ -16,64 +13,5 @@ const apiClient = axios.create({
 	},
 	withCredentials: true,
 })
-
-//NOTE: Add accesToken to every request
-apiClient.interceptors.request.use(
-	(config) => {
-		const accessToken = useAuth()
-		if (accessToken) {
-			config.headers['Authorization'] = `Bearer ${accessToken}`
-		}
-		return config
-	},
-	(error) => {
-		toast.error('An error ocurred. Please try again later')
-		return Promise.reject(error)
-	}
-)
-
-//NOTE: Refresh accesToken
-apiClient.interceptors.response.use(
-	(response) => {
-		return response
-	},
-	async (error) => {
-		//NOTE: is the error === 401 means the accessToken is expired
-		if (error.reponse && error.response.status === 401) {
-			try {
-				//NOTE: refres accesToken try
-				const response = await apiClient.post('/auth/refresh')
-
-				if (response.status === 200) {
-					const newAccessToken = response.data.accessToken
-
-					const { login } = useAuth()
-					login(newAccessToken)
-
-					//NOTE: Retry the original request with the newAccessToken
-					error.config.headers['Authorization'] =
-						`Bearer ${newAccessToken}`
-
-					//NOTE: Retry the original request
-					return axios(error.config)
-				}
-			} catch (refreshError) {
-				//NOTE: If can't refresh accessToken, redirecto no /sign-in
-				if (refreshError) {
-					toast.error('Error refresing token. Please try again later')
-				}
-
-				toast.error('An error ocurred. Please try again later')
-
-				const navigate = useNavigate()
-				navigate('/sign-in', { replace: true })
-			}
-		} else {
-			toast.error('An error ocurred. Please try again later')
-		}
-
-		return Promise.reject(error)
-	}
-)
 
 export default apiClient
