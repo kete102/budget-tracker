d7ad77f403922f87f5944236c562b14dec5025d7
Flavius Catalin Neacsu <kete102@gmail.com>
2025-03-02 21:44:56 +0100

    feat: styles added more padding

--- frontend/src/pages/dashboard-page.tsx
+++ frontend/src/pages/dashboard-page.tsx
@@ -2,7 +2,7 @@
 function DashboardPage() {
 
   return (
-    <div className="h-full text-neutral-100 w-full flex flex-col justify-center p-2">
+    <div className="h-full text-neutral-100 w-full flex flex-col justify-center p-4">
       <h1>Dashboard</h1>
       <p>Hello, flavius</p>
     </div>
