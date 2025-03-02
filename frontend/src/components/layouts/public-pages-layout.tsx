84040cbef05d07edc2fd27de476a34052961ba76
Flavius Catalin Neacsu <kete102@gmail.com>
2025-03-02 22:18:41 +0100

    refactor: changed header for header-title and added some top margin

--- frontend/src/components/layouts/public-pages-layout.tsx
+++ frontend/src/components/layouts/public-pages-layout.tsx
@@ -1,7 +1,7 @@
 import { Navigate, Outlet } from 'react-router'
-import Header from '../header'
 import Footer from '../footer'
 import { useAuth } from '@/hooks/useAuth'
+import HeaderTitle from '../header-title'
 
 const PublicPagesLayout = () => {
 	const { token } = useAuth()
@@ -9,8 +9,8 @@ const PublicPagesLayout = () => {
 	if (token) return <Navigate to='/dashboard' replace />
 
 	return (
-		<div className='h-full bg-neutral-950 flex flex-col w-full p-4'>
-			<Header />
+		<div className='h-full bg-neutral-950 flex flex-col items-center w-full p-4'>
+			<HeaderTitle styles='mt-10' />
 			<main className='h-full w-full my-2 mx-auto grow'>
 				<Outlet />
 			</main>
