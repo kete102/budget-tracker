26546a7a91a55bfcf2934a5f0df39a1069dc9165
Flavius Catalin Neacsu <kete102@gmail.com>
2025-03-02 21:43:47 +0100

    fix: styles

--- frontend/src/components/layouts/public-pages-layout.tsx
+++ frontend/src/components/layouts/public-pages-layout.tsx
@@ -9,7 +9,7 @@ const PublicPagesLayout = () => {
 	if (token) return <Navigate to='/dashboard' replace />
 
 	return (
-		<div className='h-full flex flex-col w-full p-4'>
+		<div className='h-full bg-neutral-950 flex flex-col w-full p-4'>
 			<Header />
 			<main className='h-full w-full my-2 mx-auto grow'>
 				<Outlet />
--- frontend/src/components/layouts/root-layout.tsx
+++ frontend/src/components/layouts/root-layout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react'
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
-	return <div className='h-screen bg-neutral-950 p-4'>{children}</div>
+	return <div className='h-screen w-full'>{children}</div>
 }
 
 export default RootLayout
