da8a5f38b78ee7ff1ad924647343fe269e1d3769
Flavius Catalin Neacsu <kete102@gmail.com>
2025-03-02 21:43:18 +0100

    fix: not auth navigation

--- frontend/src/components/layouts/private-pages-layout.tsx
+++ frontend/src/components/layouts/private-pages-layout.tsx
@@ -1,11 +1,14 @@
 import Header from '../header'
 import Footer from '../footer'
-import { Outlet } from 'react-router'
+import { Navigate, Outlet } from 'react-router'
+import { useAuth } from '@/hooks/useAuth'
 
 const PrivatePagesLayout = () => {
+	const { token } = useAuth()
+	if (!token) return <Navigate to='/sign-in' replace />
 
 	return (
-		<div className='h-full flex flex-col w-full p-4'>
+		<div className='h-full bg-neutral-950 flex flex-col w-full p-4'>
 			<Header />
 			<main className='container mx-auto grow'>
 				<Outlet />
