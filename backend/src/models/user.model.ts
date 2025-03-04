61ab47238f7aa8bf5ff30f8f3162a228e7afca09
Flavius Catalin Neacsu <kete102@gmail.com>
2025-03-04 22:03:53 +0100

    feat: getUserOverviewResume

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -4,6 +4,9 @@ import { userTable, PublicUser } from "../db/schemas/user.schema";
 import { db } from "../drizzle";
 import { PostgresError } from "postgres";
 import { eq } from "drizzle-orm";
+import { transactionHistoryTable } from "../db/schemas/transaction-history.schema";
+import { categoryTable } from "../db/schemas/category.schema";
+import { GetUserOverview } from "../../../shared/user/types";
 
 export const registerSchema = z.object({
   email: z.string().email(),
@@ -46,6 +49,10 @@ export class UserModel {
         id: savedUser.id,
         email: savedUser.email,
         username: savedUser.username,
+        currency: savedUser.currency,
+        balance: savedUser.balance,
+        totalExpenses: savedUser.totalExpenses,
+        totalIncome: savedUser.totalIncome,
       };
 
       return publicUser;
@@ -88,6 +95,10 @@ export class UserModel {
         id: existingUser.id,
         email: existingUser.email,
         username: existingUser.username,
+        currency: existingUser.currency,
+        balance: existingUser.balance,
+        totalExpenses: existingUser.totalExpenses,
+        totalIncome: existingUser.totalIncome,
       };
 
       return publicUser;
@@ -99,5 +110,41 @@ export class UserModel {
       throw new Error("Unexpected error");
     }
   }
-  static async logout() {}
+  static async getUserOverview({
+    userId,
+  }: {
+    userId: string;
+  }): Promise<GetUserOverview> {
+    try {
+      const [userResume] = await db
+        .select({
+          username: userTable.username,
+          email: userTable.email,
+          balance: userTable.balance,
+          totalIncome: userTable.totalIncome,
+          totalExpenses: userTable.totalExpenses,
+        })
+        .from(userTable)
+        .where(eq(userTable.id, userId));
+
+      const userTransactions = await db
+        .select()
+        .from(transactionHistoryTable)
+        .where(eq(transactionHistoryTable.userId, userId));
+
+      const userCategories = await db
+        .select()
+        .from(categoryTable)
+        .where(eq(categoryTable.userId, userId));
+
+      return { userResume, userTransactions, userCategories };
+    } catch (error) {
+      console.log(error);
+      if (error instanceof PostgresError) {
+        throw new Error(`Postgres error ${error.message}`);
+      }
+
+      throw new Error("Unexpected error");
+    }
+  }
 }
