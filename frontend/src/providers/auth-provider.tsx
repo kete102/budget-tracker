bf49867bd1db0f5ef71f47e55e8716697fa8b1fe
Flavius Catalin Neacsu <kete102@gmail.com>
2025-03-04 18:10:41 +0100

    fix: endpoint changed

--- frontend/src/providers/auth-provider.tsx
+++ frontend/src/providers/auth-provider.tsx
@@ -16,7 +16,7 @@ const AuthProvider = ({ children }: { children: ReactNode }) => {
 	useEffect(() => {
 		const fetchMe = async () => {
 			try {
-				const response = await apiClient.get('/auth/check-auth')
+				const response = await apiClient.get('/user/check-auth')
 				setToken(response.data.accessToken)
 			} catch {
 				setToken(null)
@@ -53,7 +53,7 @@ const AuthProvider = ({ children }: { children: ReactNode }) => {
 					originalRequest._retry = true
 
 					try {
-						const response = await apiClient.get('/auth/refresh')
+						const response = await apiClient.get('/user/refresh')
 
 						setToken(response.data.accessToken)
 
