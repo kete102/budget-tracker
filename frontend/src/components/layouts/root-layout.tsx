88e5163ca3e455b5e5059be6e798d8bf2908ceb6
Flavius Catalin Neacsu <kete102@gmail.com>
2025-03-05 17:05:40 +0100

    feat: added sidebar

--- frontend/src/components/layouts/root-layout.tsx
+++ frontend/src/components/layouts/root-layout.tsx
@@ -1,7 +1,39 @@
+import { useSidebar } from '@/hooks/use-sidebar'
+import { ThemeProvider } from '@/providers/theme-provider'
+import { Menu } from 'lucide-react'
 import { ReactNode } from 'react'
+import Footer from '../footer'
+import HeaderTitle from '../header-title'
+import { ModeToggle } from '../mode-toogle'
+import Sidebar from '../sidebar'
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
-	return <div className="h-screen w-full">{children}</div>
+	const { toggleSidebar, isSidebarOpen } = useSidebar()
+
+	return (
+		<ThemeProvider defaultTheme="dark">
+			<div className="flex h-screen w-full">
+				<Sidebar
+					isOpen={isSidebarOpen}
+					toggleSidebar={toggleSidebar}
+				/>
+				<div className="flex h-full w-full flex-col items-center p-2 dark:bg-neutral-950">
+					<div className="flex w-full items-center justify-between p-2">
+						<button
+							onClick={() => toggleSidebar()}
+							className="cursor-pointer rounded-md border border-neutral-500 p-1 dark:border-neutral-800"
+						>
+							<Menu />
+						</button>
+						<HeaderTitle />
+						<ModeToggle />
+					</div>
+					<main className="my-2 h-full w-full">{children}</main>
+					<Footer />
+				</div>
+			</div>
+		</ThemeProvider>
+	)
 }
 
 export default RootLayout
