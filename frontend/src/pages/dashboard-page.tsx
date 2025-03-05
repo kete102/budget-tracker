0ac2e2b78d52d187f8719720ff0d1e327f9fbd55
Flavius Catalin Neacsu <kete102@gmail.com>
2025-03-05 16:32:03 +0100

    core: added motion

--- frontend/src/pages/dashboard-page.tsx
+++ frontend/src/pages/dashboard-page.tsx
@@ -1,8 +1,22 @@
+import { useUserResume } from '@/hooks/use-user-service'
+
 function DashboardPage() {
+	const { data, isFetching } = useUserResume()
+
+	if (!data) {
+		return <div className="text-neutral-100">No username</div>
+	}
+
+	if (isFetching) {
+		return <div className="text-neutral-100">Loading...</div>
+	}
+
 	return (
-		<div className="flex h-full w-full flex-col justify-center p-4 text-neutral-100">
-			<h1>Dashboard</h1>
-			<p>Hello, flavius</p>
+		<div className="flex h-full w-full flex-col p-2">
+			<h1 className="text-2xl font-bold text-neutral-800 dark:text-neutral-100">
+				Hi! {data.userResume.username} 👋
+			</h1>
+			<section></section>
 		</div>
 	)
 }
