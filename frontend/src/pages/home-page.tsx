fd7581b1564eddc066f81c1f008462324b6510b1
Flavius Catalin Neacsu <kete102@gmail.com>
2025-03-05 16:18:44 +0100

    styles: dark mode

--- frontend/src/pages/home-page.tsx
+++ frontend/src/pages/home-page.tsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router'
 
 function HomePage() {
 	return (
-		<div className="flex h-full w-full flex-col items-center justify-center bg-gradient-to-br from-neutral-950 to-neutral-900 p-4">
+		<div className="flex h-full w-full flex-col items-center justify-center">
 			<HeaderTitle styles="text-3xl md:text-5xl lg:text-8xl" />
 			<section className="mt-4 w-full max-w-3xl space-y-6 text-center">
-				<h2 className="text-md text-neutral-100 md:text-lg lg:text-xl">
+				<h2 className="text-md text-neutral-900 md:text-lg lg:text-xl dark:text-neutral-100">
 					Track your finances effortlessly. Gain control over your spending.
 				</h2>
 				<div className="flex w-full items-center justify-center gap-x-6">
@@ -15,7 +15,7 @@ function HomePage() {
 						to="/dashboard"
 						viewTransition
 					>
-						<button className="text-md inline-flex cursor-pointer items-center gap-x-2 rounded-lg bg-neutral-100 px-4 py-2 font-semibold text-neutral-900 transition-transform hover:scale-105 active:scale-95 md:text-lg lg:text-xl">
+						<button className="text-md inline-flex cursor-pointer items-center gap-x-2 rounded-lg bg-neutral-900 px-4 py-2 font-semibold text-neutral-100 transition-transform hover:scale-105 active:scale-95 md:text-lg lg:text-xl dark:bg-neutral-100 dark:text-neutral-900">
 							Get started <MoveRight />
 						</button>
 					</Link>
