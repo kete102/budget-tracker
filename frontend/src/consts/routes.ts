5191f886ed8bb0dc1c5c62b61dcf99c37ae74818
Flavius Catalin Neacsu <kete102@gmail.com>
2025-03-05 16:17:00 +0100

    refactor: route consts

---
+++ frontend/src/consts/routes.ts
@@ -0,0 +1,22 @@
+export const routes = [
+	{
+		path: '/dashboard',
+		name: 'Dashboard',
+	},
+	{
+		path: '/transactions',
+		name: 'Transactions',
+	},
+	{
+		path: '/budgets',
+		name: 'Budgets',
+	},
+	{
+		path: '/reports',
+		name: 'Reports',
+	},
+	{
+		path: '/settings',
+		name: 'Settings',
+	},
+]
