a05aca1809b77d91ee7407e122cdabce47fda8c4
Flavius Catalin Neacsu <kete102@gmail.com>
2025-03-05 22:35:47 +0100

    core: currencies constant

---
+++ frontend/src/consts/currencies.ts
@@ -0,0 +1,62 @@
+export const CURRENCIES = [
+	{
+		name: 'US Dollar',
+		value: 'usd',
+		symbol: '$',
+		flag: '🇺🇸',
+	},
+	{
+		name: 'Euro',
+		value: 'eur',
+		symbol: '€',
+		flag: '🇪🇺',
+	},
+	{
+		name: 'British Pound',
+		value: 'gbp',
+		symbol: '£',
+		flag: '🇬🇧',
+	},
+	{
+		name: 'Japanese Yen',
+		value: 'jpy',
+		symbol: '¥',
+		flag: '🇯🇵',
+	},
+	{
+		name: 'Canadian Dollar',
+		value: 'cad',
+		symbol: '$',
+		flag: '🇨🇦',
+	},
+	{
+		name: 'Australian Dollar',
+		value: 'aud',
+		symbol: '$',
+		flag: '🇦🇺',
+	},
+	{
+		name: 'Swiss Fran',
+		value: 'chf',
+		symbol: 'CHF',
+		flag: '🇨🇭',
+	},
+	{
+		name: 'Chinese Yuan',
+		value: 'cny',
+		symbol: '¥',
+		flag: '🇨🇳',
+	},
+	{
+		name: 'Mexican Peso',
+		value: 'mxn',
+		symbol: '$',
+		flag: '🇲🇽',
+	},
+	{
+		name: 'Brazilian Real',
+		value: 'brl',
+		symbol: 'R$',
+		flag: '🇧🇷',
+	},
+]
