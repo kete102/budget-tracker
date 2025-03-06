4eecdaaf455c192584d5ce17b85d9c76b2ccb496
Flavius Catalin Neacsu <kete102@gmail.com>
2025-03-06 01:38:03 +0100

    feat: update users schemas

--- backend/src/db/schemas/user.schema.ts
+++ backend/src/db/schemas/user.schema.ts
@@ -1,11 +1,21 @@
 import { InferSelectModel } from "drizzle-orm";
-import { pgTable, text, uuid } from "drizzle-orm/pg-core";
+import { numeric, pgTable, text, uuid } from "drizzle-orm/pg-core";
 
-export const userTable = pgTable("user", {
+export const userTable = pgTable("users", {
   id: uuid("id").primaryKey().defaultRandom(),
   username: text("username").unique().notNull(),
   email: text("email").unique().notNull(),
   password_hash: text("password_hash").notNull(),
+  currency: text("currency").default("USD").notNull(),
+  balance: numeric("balance", { precision: 20, scale: 2 })
+    .notNull()
+    .default("0"),
+  totalIncome: numeric("total_income", { precision: 20, scale: 2 })
+    .notNull()
+    .default("0"),
+  totalExpenses: numeric("total_expenses", { precision: 20, scale: 2 })
+    .notNull()
+    .default("0"),
 });
 
 export type User = InferSelectModel<typeof userTable>;
