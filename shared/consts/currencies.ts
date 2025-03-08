f41b506d277d8aa2a71cd4803ae13514bf44310d
Flavius Catalin Neacsu <kete102@gmail.com>
2025-03-08 15:40:55 +0100

    refactor: moved CURRENCIES to /shared

---
+++ shared/consts/currencies.ts
@@ -0,0 +1,62 @@
+export const CURRENCIES = [
+  {
+    name: "US Dollar",
+    value: "USD",
+    symbol: "$",
+    flag: "🇺🇸",
+  },
+  {
+    name: "Euro",
+    value: "EUR",
+    symbol: "€",
+    flag: "🇪🇺",
+  },
+  {
+    name: "British Pound",
+    value: "GBP",
+    symbol: "£",
+    flag: "🇬🇧",
+  },
+  {
+    name: "Japanese Yen",
+    value: "JPY",
+    symbol: "¥",
+    flag: "🇯🇵",
+  },
+  {
+    name: "Canadian Dollar",
+    value: "CAD",
+    symbol: "$",
+    flag: "🇨🇦",
+  },
+  {
+    name: "Australian Dollar",
+    value: "AUD",
+    symbol: "$",
+    flag: "🇦🇺",
+  },
+  {
+    name: "Swiss Fran",
+    value: "CHF",
+    symbol: "CHF",
+    flag: "🇨🇭",
+  },
+  {
+    name: "Chinese Yuan",
+    value: "CNY",
+    symbol: "¥",
+    flag: "🇨🇳",
+  },
+  {
+    name: "Mexican Peso",
+    value: "MXN",
+    symbol: "$",
+    flag: "🇲🇽",
+  },
+  {
+    name: "Brazilian Real",
+    value: "BRL",
+    symbol: "R$",
+    flag: "🇧🇷",
+  },
+];
