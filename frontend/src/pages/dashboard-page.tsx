37ada040a8142e9913d4afbcd284e019878dc615
Flavius Catalin Neacsu <kete102@gmail.com>
2025-03-08 22:08:43 +0100

    fix: styles and card details

--- frontend/src/pages/dashboard-page.tsx
+++ frontend/src/pages/dashboard-page.tsx
@@ -3,19 +3,13 @@ import {
 	Card,
 	CardContent,
 	CardDescription,
-	CardFooter,
 	CardHeader,
 	CardTitle,
 } from '@/components/ui/card'
-import {
-	ChartConfig,
-	ChartContainer,
-	ChartTooltip,
-	ChartTooltipContent,
-} from '@/components/ui/chart'
 import UserCurrency from '@/components/user-currency'
 import { CURRENCIES } from '@/consts/currencies'
 import { useUserResume } from '@/hooks/use-user-service'
+import { useUserTransactions } from '@/hooks/use-user-transactions'
 import {
 	Activity,
 	ArrowRight,
@@ -27,16 +21,19 @@ import {
 	TrendingUp,
 } from 'lucide-react'
 import { Link } from 'react-router'
-import { Bar, BarChart, CartesianGrid, XAxis } from 'recharts'
 
 function DashboardPage() {
-	const { data, isLoading } = useUserResume()
+	const { data: userOverview, isLoading: loadingUserOverview } = useUserResume()
+	const { data: transactions, isLoading: loadingTransactions } =
+		useUserTransactions()
 
-	if (!data) {
+	console.log(transactions)
+
+	if (!userOverview) {
 		return <div className="text-foreground">No username</div>
 	}
 
-	if (isLoading) {
+	if (loadingUserOverview) {
 		return (
 			<div className="grid h-full w-full place-content-center">
 				<Ellipsis className="size-10" />
@@ -45,148 +42,115 @@ function DashboardPage() {
 	}
 
 	const userCurrency = CURRENCIES.find(
-		(currency) => currency.value === data.userResume.currency
+		(currency) => currency.value === userOverview.userResume.currency
 	)
 
-	const chartData = [
-		{ month: 'January', desktop: 186, mobile: 80 },
-		{ month: 'February', desktop: 305, mobile: 200 },
-		{ month: 'March', desktop: 237, mobile: 120 },
-		{ month: 'April', desktop: 73, mobile: 190 },
-		{ month: 'May', desktop: 209, mobile: 130 },
-		{ month: 'June', desktop: 214, mobile: 140 },
-	]
-
-	const chartConfig = {
-		desktop: {
-			label: 'Desktop',
-			color: 'hsl(var(--chart-1))',
-		},
-		mobile: {
-			label: 'Mobile',
-			color: 'hsl(var(--chart-2))',
-		},
-	} satisfies ChartConfig
-
 	return (
-		<div className="flex h-full w-full flex-col gap-y-4 p-2">
-			<section className="flex w-full items-center justify-between">
-				<h1 className="text-foreground text-2xl font-bold">
-					Hi! {data.userResume.username} 👋
-				</h1>
-				<UserCurrency />
-			</section>
+		<div className="h-full w-full p-2">
+			<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
+				{/* Saludo y Balance */}
+				<article className="col-span-1 sm:col-span-2 lg:col-span-2 xl:col-span-3">
+					<section className="flex w-full flex-col gap-4">
+						<div className="flex w-full items-center justify-between">
+							<h1 className="text-foreground text-2xl font-bold">
+								Hi! {userOverview.userResume.username} 👋
+							</h1>
+							<UserCurrency />
+						</div>
+						<div className="flex items-center justify-between">
+							<h2 className="inline-flex items-center gap-x-2 text-xl font-semibold text-amber-600">
+								<PiggyBank /> Total Balance
+							</h2>
+							<h1 className="text-foreground text-2xl font-bold">
+								{userOverview.userResume.balance} {userCurrency?.symbol}
+							</h1>
+						</div>
+					</section>
+				</article>
 
-			<section className="flex w-full flex-col justify-between gap-y-2 md:flex-row md:gap-x-1">
-				<Button
-					variant="default"
-					className="md:w-1/2"
-				>
-					<SquareArrowDownRight />
-					New income
-				</Button>
-				<Button
-					variant="secondary"
-					className="md:w-1/2"
-				>
-					<SquareArrowUpRight />
-					New expense
-				</Button>
-			</section>
-
-			<section className="grid grid-cols-4 grid-rows-4 gap-2 md:grid-rows-6">
-				<Card className="col-span-2 bg-neutral-900/10 dark:bg-white/5">
-					<CardContent>
-						<h2
-							className={`text-foreground inline-flex items-center gap-x-2 text-2xl font-bold`}
-						>
-							{data.userResume.balance} {userCurrency?.symbol}
-						</h2>
-					</CardContent>
-					<CardHeader>
-						<CardTitle className="text-muted-foreground flex items-center gap-x-2">
-							<PiggyBank />
-							Total Balance
-						</CardTitle>
-					</CardHeader>
-				</Card>
-
-				<Card className="bg-background col-span-2 col-start-3"></Card>
-
-				<Card className="bg-background col-span-2 row-start-2 w-full">
-					<CardContent>
-						<h2 className="inline-flex items-center gap-x-2 text-2xl font-bold text-green-600">
-							+ {data.userResume.totalIncome} {userCurrency?.symbol}
-						</h2>
-					</CardContent>
-					<CardHeader className="flex w-full flex-col items-start justify-between md:flex-row md:items-center">
-						<CardTitle className="flex items-center gap-x-2">
-							<TrendingUp />
-							Total Incomes
-						</CardTitle>
-						<Button
-							variant="link"
-							asChild
-							className="text-muted-foreground"
-						>
-							<Link
-								viewTransition
-								to="/transactions?filter=income"
-							>
-								See all <ArrowRight />{' '}
-							</Link>
-						</Button>
-					</CardHeader>
-				</Card>
-
-				<Card className="bg-background col-span-2 col-start-3 row-start-2 w-full">
-					<CardContent>
-						<h2 className="inline-flex items-center gap-x-2 text-2xl font-bold text-red-600">
-							- {data.userResume.totalExpenses} {userCurrency?.symbol}
-						</h2>
-					</CardContent>
-					<CardHeader className="flex w-full flex-col items-start justify-between md:flex-row md:items-center">
-						<CardTitle className="flex items-center gap-x-2">
-							<TrendingDown /> Total Expenses
-						</CardTitle>
-						<Button
-							variant="link"
-							asChild
-							className="text-muted-foreground"
-						>
-							<Link
-								viewTransition
-								to="/transactions?filter=expense"
-							>
-								See all <ArrowRight />{' '}
-							</Link>
-						</Button>
-					</CardHeader>
-				</Card>
-
-				<Card className="bg-background col-span-4 row-span-2 row-start-3 md:row-span-4">
-					<CardHeader className="flex w-full flex-row items-center justify-between">
-						<CardTitle className="flex items-center gap-x-2">
-							<Activity /> Activity
-						</CardTitle>
-						<CardDescription>
+				{/* Ingresos y Gastos */}
+				<article className="col-span-full flex w-full flex-row items-center gap-x-2">
+					<Card className="bg-background w-full">
+						<CardContent>
+							<h2 className="text-foreground text-2xl font-bold">
+								+ {userOverview.userResume.totalIncome} {userCurrency?.symbol}
+							</h2>
+						</CardContent>
+						<CardHeader className="flex w-full justify-between">
+							<CardTitle className="text-muted-foreground flex items-center gap-x-2">
+								<TrendingUp className="size-5" /> Total Incomes
+							</CardTitle>
+						</CardHeader>
+						<CardContent className="w-full">
 							<Button
-								variant="link"
-								asChild
-								className="text-muted-foreground"
+								variant="outline"
+								className="w-full border border-green-900 bg-emerald-600"
 							>
-								<Link
-									viewTransition
-									to="/transactions"
-								>
-									See all <ArrowRight />{' '}
-								</Link>
+								<SquareArrowDownRight /> New income
 							</Button>
-						</CardDescription>
-					</CardHeader>
-					<CardContent></CardContent>
-				</Card>
-			</section>
+						</CardContent>
+					</Card>
+
+					<Card className="bg-background w-full">
+						<CardContent>
+							<h2 className="text-foreground text-2xl font-bold">
+								- {userOverview.userResume.totalExpenses} {userCurrency?.symbol}
+							</h2>
+						</CardContent>
+						<CardHeader className="flex w-full justify-between">
+							<CardTitle className="text-muted-foreground flex items-center gap-x-2">
+								<TrendingDown className="size-5" /> Total Expenses
+							</CardTitle>
+						</CardHeader>
+						<CardContent className="w-full">
+							<Button
+								variant="outline"
+								className="w-full border border-red-900 bg-red-500"
+							>
+								<SquareArrowUpRight /> New expense
+							</Button>
+						</CardContent>
+					</Card>
+				</article>
+
+				{/* Actividad */}
+				<article className="col-span-1 sm:col-span-2 lg:col-span-2 xl:col-span-3">
+					<Card className="bg-background h-full">
+						<CardHeader className="flex w-full justify-between">
+							<CardTitle className="flex items-center gap-x-2">
+								<Activity /> Activity
+							</CardTitle>
+							<CardDescription>
+								<Button
+									variant="link"
+									asChild
+									className="text-muted-foreground"
+								>
+									<Link
+										viewTransition
+										to="/transactions"
+									>
+										See all <ArrowRight />
+									</Link>
+								</Button>
+							</CardDescription>
+						</CardHeader>
+						<CardContent>
+							<span className="text-muted-foreground text-sm">
+								Showing last transactions
+							</span>
+							<div className="mt-4">
+								{loadingTransactions && <p>Loading...</p>}
+								{transactions.length === 0 && (
+									<p className="text-muted-foreground text-center font-semibold">
+										No transactions yet
+									</p>
+								)}
+							</div>
+						</CardContent>
+					</Card>
+				</article>
+			</div>
 		</div>
 	)
 }
